af4a7c3f812e57e377b7b0fd85a9e3945555b160
mavvy22 <mavvystudio@gmail.com>
2024-03-01 21:28:08 +0800

    refactor: move functions from App to HeaderControl

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import * as constants from '../constants';
-import * as utils from '../utils';
 import useZip from '../use-zip.js';
 import styles from './App.module.css';
 
@@ -13,13 +12,7 @@ const App = () => {
   const [processFileIndex, setProcessFileIndex] = useState(null);
   const [phase, setPhase] = useState(constants.phase.waiting);
   const { entries, copy, rename, setEntries } = useZip();
-  const [dirPicker, setDirPicker] = useState(null);
   const [alert, setAlert] = useState(null);
-
-  const handleExtractFile = useCallback(
-    (item) => utils.extractFile(item, dirPicker),
-    [dirPicker],
-  );
 
   const started = phase !== constants.phase.waiting;
   const isDone = phase === constants.phase.done;
@@ -33,84 +26,22 @@ const App = () => {
     }
   }, [isDone]);
 
-  useEffect(() => {
-    (async () => {
-      if (
-        phase === constants.phase.waiting ||
-        phase === constants.phase.paused
-      ) {
-        return false;
-      }
-
-      const item = entries[processFileIndex || 0];
-
-      if (!item) {
-        setPhase(constants.phase.done);
-        return false;
-      }
-
-      try {
-        await handleExtractFile(item);
-      } catch (e) {
-        setPhase(constants.phase.paused);
-        setAlert({
-          title: 'Error',
-          message:
-            'Something went wrong. Please check the contents of the zip file.',
-        });
-        return false;
-      }
-
-      setProcessFileIndex(processFileIndex + 1);
-    })();
-  }, [
-    handleExtractFile,
-    phase,
-    entries,
-    processFileIndex,
-    setProcessFileIndex,
-  ]);
-
   const handleClose = () => {
     setAlert(null);
   };
 
   const defaultProcessIndex = processFileIndex || 0;
 
-  const handleReset = () => {
-    setEntries(null);
-    setProcessFileIndex(null);
-    setPhase(constants.phase.waiting);
-  };
-
-  const handleUnZip = async () => {
-    if (started) {
-      return false;
-    }
-    try {
-      const dirPicker = await window.showDirectoryPicker();
-      setDirPicker(dirPicker);
-      setPhase(constants.phase.processing);
-    } catch (e) {}
-  };
-
-  const handlePause = () => {
-    const value =
-      phase === constants.phase.paused
-        ? constants.phase.processing
-        : constants.phase.paused;
-    setPhase(value);
-  };
-
   return (
     <div className={styles.root}>
       <HeaderControl
-        onUnZip={handleUnZip}
-        onReset={handleReset}
-        onPause={handlePause}
-        setEntries={setEntries}
-        phase={phase}
         entries={entries}
+        setEntries={setEntries}
+        processFileIndex={processFileIndex}
+        setProcessFileIndex={setProcessFileIndex}
+        phase={phase}
+        setPhase={setPhase}
+        setAlert={setAlert}
       />
       <Entries
         entries={entries}
--- src/components/HeaderControl.js
+++ src/components/HeaderControl.js
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
+import * as utils from '../utils';
 import * as constants from '../constants';
 import styles from './HeaderControl.module.css';
 import AppInput from './AppInput';
 import Button from './Button';
 
 const HeaderControl = (props) => {
+  const [dirPicker, setDirPicker] = useState(null);
+
+  const handleExtractFile = useCallback(
+    (item) => utils.extractFile(item, dirPicker),
+    [dirPicker],
+  );
+  useEffect(() => {
+    (async () => {
+      if (
+        props.phase === constants.phase.waiting ||
+        props.phase === constants.phase.paused
+      ) {
+        return false;
+      }
+
+      const item = props.entries[props.processFileIndex || 0];
+
+      if (!item) {
+        props.setPhase(constants.phase.done);
+        return false;
+      }
+
+      try {
+        await handleExtractFile(item);
+      } catch (e) {
+        props.setPhase(constants.phase.paused);
+        props.setAlert({
+          title: 'Error',
+          message:
+            'Something went wrong. Please check the contents of the zip file.',
+        });
+        return false;
+      }
+
+      props.setProcessFileIndex(props.processFileIndex + 1);
+    })();
+  }, [handleExtractFile, props]);
+
   const started = props.phase !== constants.phase.waiting;
   const isDone = props.phase === constants.phase.done;
   const controlText =
@@ -13,19 +52,44 @@ const HeaderControl = (props) => {
   const emptyEntries = !props.entries?.length;
   const showPauseResumeBtn = started && !isDone;
 
+  const handlePause = () => {
+    const value =
+      props.phase === constants.phase.paused
+        ? constants.phase.processing
+        : constants.phase.paused;
+    props.setPhase(value);
+  };
+
+  const handleUnZip = async () => {
+    if (started) {
+      return false;
+    }
+    try {
+      const dirPicker = await window.showDirectoryPicker();
+      setDirPicker(dirPicker);
+      props.setPhase(constants.phase.processing);
+    } catch (e) {}
+  };
+
+  const handleReset = () => {
+    props.setEntries(null);
+    props.setProcessFileIndex(null);
+    props.setPhase(constants.phase.waiting);
+  };
+
   return (
     <div className={styles.root}>
-      <AppInput onReset={props.onReset} setEntries={props.setEntries} />
+      <AppInput onReset={handleReset} setEntries={props.setEntries} />
       <div className={styles.control}>
         <Button
           disabled={emptyEntries || started}
-          onClick={props.onUnZip}
+          onClick={handleUnZip}
           primary
         >
           Unzip
         </Button>
         {showPauseResumeBtn && (
-          <Button disabled={emptyEntries} onClick={props.onPause}>
+          <Button disabled={emptyEntries} onClick={handlePause}>
             {controlText}
           </Button>
         )}
